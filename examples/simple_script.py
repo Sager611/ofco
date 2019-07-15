f188d32da8c2aca18b6d52a92ee5acd7adaea10e
faymanns <florian.aymanns@epfl.ch>
2019-07-15 18:21:02 +0200

    added simple example

---
+++ examples/simple_script.py
@@ -0,0 +1,11 @@
+from skimage import io
+from ofco import motion_compensate
+from ofco.utils import default_parameters
+
+stack1 = io.imread("/mnt/internal_hdd/aymanns/181220_Rpr_R57C10_GC6s_tdTom/Fly5/002_coronal/2p/red.tif")
+stack2 = io.imread("/mnt/internal_hdd/aymanns/181220_Rpr_R57C10_GC6s_tdTom/Fly5/002_coronal/2p/red.tif")
+
+param = default_parameters()
+frames = range(2)
+
+motion_compensate(stack1, stack2, 'warped1.tif', 'warped2.tif', frames, param)
