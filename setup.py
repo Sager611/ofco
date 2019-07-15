57f9e42f08feae3fd9a20a8a5ce9eebfc2fc4524
faymanns <florian.aymanns@epfl.ch>
2019-07-15 15:20:05 +0200

    setup file with readme integration

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ofco",
+    version='0.1',
+    packages=["ofco"],
+    entry_points={"console_scripts": ["ofco = ofco.main:main"]},
+    author="Florian Aymanns",
+    author_email="florian.ayamnns@epfl.ch",
+    description="Optic flow based motion correction for Calcium two-photon images.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NeLy-EPFL/ofco",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "scikit-image",
+        "opencv-python",
+    ],
+)
