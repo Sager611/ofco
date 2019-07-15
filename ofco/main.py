b3b0b6697c701df4b33d619e45e88c62e2070158
faymanns <florian.aymanns@epfl.ch>
2019-07-15 15:53:58 +0200

    initial commit

---
+++ ofco/main.py
@@ -0,0 +1,135 @@
+import os
+import multiprocessing
+from timeit import default_timer as timer
+import numpy as np
+from skimage import io
+from numba import jit, prange
+
+from .utils import default_parameters, midway, crop_fit_size_center
+from .warping import bilinear_interpolate
+from .of_l1_l2_fm_admm import of_l1_l2_fm_admm
+
+
+def compute_motion(I1, I2, param):
+    sz0 = I1.shape
+    
+    I1 = np.pad(I1, [15,15], 'edge')
+    I2 = np.pad(I2, [15,15], 'edge')
+    
+    #Optical flow
+    w = of_l1_l2_fm_admm(I1, I2, sz0, param)
+
+    w = crop_fit_size_center(w, [sz0[0], sz0[1], 2])
+    return w
+            
+
+def parallel_compute_motion(t):
+    i2 = global_stack1_rescale[t + 1,:,:]
+    [i10, i2] = midway(global_i1, i2)
+    return compute_motion(i10, i2, global_param)
+
+
+def motion_compensate(stack1, stack2, output1, output2, frames, param, verbose=False, parallel=True):     
+    start = timer()
+    stack1 = stack1[frames,:,:].astype(np.float64)
+    stack1_rescale = (stack1 - np.amin(stack1)) / (np.amax(stack1) - np.amin(stack1)) * 255
+    stack2 = stack2[frames,:,:].astype(np.float64)
+    end = timer()
+    if verbose:
+        print('Time it took to normalize images {}'.format(end-start))
+
+    # Motion estimation
+    start = timer()
+    w = np.zeros([stack1_rescale.shape[0] - 1, stack1_rescale.shape[1], stack1_rescale.shape[2], 2])
+    
+    i1 = stack1_rescale[frames[0], :, :]
+    if parallel:
+        global global_stack1_rescale
+        global_stack1_rescale = stack1_rescale
+        global global_i1
+        global_i1 = i1
+        global global_param
+        global_param = param
+        pool = multiprocessing.Pool()
+        output = pool.map(parallel_compute_motion, range(len(frames) - 1))
+        w = np.array(output)
+    else:
+        for t in range(len(frames) - 1):
+            if verbose:
+                print('Frame {}\n'.format(t))
+            i2 = stack1_rescale[t + 1,:,:]
+            [i10, i2] = midway(i1, i2)
+            w[t, :, :, :] = compute_motion(i10, i2, param)
+    end = timer()
+    if verbose:
+        print('Time it took to compute motion field w {}'.format(end - start))
+    
+    start = timer()
+    stack1_warped = stack1
+    stack2_warped = stack2
+    for t in range(len(frames) - 1):
+        stack1_warped[t + 1, :, :] = bilinear_interpolate(stack1[t + 1, :, :], w[t, :, :, 0], w[t, :, :, 1])
+        stack2_warped[t + 1, :, :] = bilinear_interpolate(stack2[t + 1, :, :], w[t, :, :, 0], w[t, :, :, 1])
+    end = timer()
+    if verbose:
+        print('Time it took to warp images {}'.format(end - start))
+
+    start = timer()
+    io.imsave(output1, stack1_warped.astype(np.float32))
+    io.imsave(output2, stack2_warped.astype(np.float32))
+    end = timer()
+    if verbose:
+        print('Time it took to save images {}'.format(end - start))
+
+def main(stack1, stack2, output_dir, frames=-1, verbose=False, **kwargs):
+    """
+    Parameters
+    ----------
+    stack1 : string
+        Path to stack with constant brightness (e.g. tdTom).
+    stack2 : string
+        Path to stack with functional information (e.g. GCamP).
+    output_dir : string
+        Path to the output directory. If it does not exist it is
+        created.
+    frames : int or list of int, optional
+        Frames that are motion corrected.
+        Default is -1, which means all frames in the stack are considered.
+    verbose : boolean
+        Default False.
+
+    Additional keyword arguments can be used to change the parameters of the
+    algorithm. For a description of the parameters see the default_parameters.
+    """
+    # Parameters
+    param = default_parameters()
+    for key, value in kwargs.items():
+        param[key] = value
+    
+    start = timer() 
+    stack1 = io.imread(stack1)
+    stack2 = io.imread(stack2)
+    end = timer()
+    if verbose:
+        print('Time it took to load images {}'.format(end - start))
+
+    assert(stack1.shape == stack2.shape)
+
+    if frames == -1:
+        frames = range(len(stack1))
+    elif type(frames) == int:
+        assert(frames <= stack1.shape[0])
+        frames = range(frames)
+
+
+    os.makedirs(output_dir, exist_ok=True)
+    
+    output1 = os.path.join(output_dir, 'warped1.tif')
+    output2 = os.path.join(output_dir, 'warped2.tif')
+     
+    motion_compensate(stack1, stack2, output1, output2, frames, param, verbose, parallel=False)
+
+if __name__ == "__main__":
+    main('/mnt/internal_hdd/aymanns/181220_Rpr_R57C10_GC6s_tdTom/Fly5/002_coronal/2p/red.tif',
+         '/mnt/internal_hdd/aymanns/181220_Rpr_R57C10_GC6s_tdTom/Fly5/002_coronal/2p/green.tif',
+         'results/test_181220_Fly5_002_module', verbose=True, frames=2)
---
+++ ofco/utils.py
@@ -0,0 +1,229 @@
+import numpy as np
+from scipy.signal import correlate2d, medfilt2d
+from scipy.ndimage.morphology import binary_dilation
+import cv2
+
+from .warping import interp2_bicubic
+
+
+def partial_deriv(imgs, w, deriv_filter=np.array([[1, -8, 0, 8, -1]])/12):
+    xx, yy = np.meshgrid(np.arange(imgs.shape[1]),np.arange(imgs.shape[0]))
+    xi = xx + w[:, :, 0]
+    yi = yy + w[:, :, 1]
+    [warped_img, Ix, Iy] = interp2_bicubic(imgs[:, :, 1], xi, yi, deriv_filter)
+    indx = np.isnan(warped_img)
+    It = warped_img - imgs[:, :, 0]
+    It[indx] = 0
+    I1x = correlate2d(imgs[:, :, 0], deriv_filter, boundary='symm', mode='same')
+    I1y = correlate2d(imgs[:, :, 0], deriv_filter.transpose(), boundary='symm', mode='same')
+    b = 0.5
+    Ix = b * Ix + (1 - b) * I1x
+    Iy = b * Iy + (1 - b) * I1y
+    Ix[indx] = 0
+    Iy[indx] = 0
+
+    return It, Ix, Iy
+
+
+def weighted_median(w, u):
+    """
+    compute the weighted median
+    uo = \min_u \sum w(i)|uo - u(i)|
+    using the formula (3.13) in Y. Li and Osher
+    "A New Median Formula with Applications to PDE Based Denoising"
+    applied to every corresponding columns of w and u
+    """
+    H, W = u.shape
+    ir = np.argsort(u, axis=0)
+    ic = np.tile(np.arange(W), [H, 1])
+    # sorting again is faster than using indices
+    sort_u = np.sort(u, axis=0)
+    w = w[(ir, ic)]
+    k = np.ones((1, W))
+    pp = -1 * np.sum(w, axis=0)
+    for i in range(H - 1, -1 , -1):
+        pc = pp + 2 * w[i, :]
+        indx = np.logical_and(pc >= 0, pp <=0)
+        k[:, indx] = H - i
+        pp = pc
+    k = H - k
+    uo = sort_u[(k.astype(np.int), np.arange(W))]
+    return uo
+
+
+def denoise_color_weighted_medfilt2d(uv, im, occ=None, bfhsz=None, mfsz=None, sigma_i=5, sigma_x=20, fullVersion=False):
+    """
+    edge region: weighted median filtering, the weights are determined by
+    spatial distance, intensity distance, occlusion state
+    smooth region: 
+    """
+    dilate_sz = [5, 5]  # dilation window size for flow edge region [5 5]
+    
+    sz = im.shape
+    sz = sz[:2]
+    
+    if occ is None:
+        occ = ones(sz);
+    
+    if bfhsz is None:
+        bfhsz = 10 # half window size
+    
+    if mfsz is None:
+        uvo = uv 
+    else:
+        mfsz = int(mfsz)
+        uvo = np.zeros_like(uv)
+        padded_uv = np.pad(uv, ((mfsz, mfsz), (mfsz, mfsz), (0, 0)), 'symmetric')
+        uvo[:,:,0] = medfilt2d(padded_uv[:,:,0], mfsz)[mfsz:-mfsz, mfsz:-mfsz]
+        uvo[:,:,1] = medfilt2d(padded_uv[:,:,1], mfsz)[mfsz:-mfsz, mfsz:-mfsz]
+
+    sobelx = cv2.Sobel(uv[:,:,0], cv2.CV_64F, 1, 0)
+    sobely = cv2.Sobel(uv[:,:,0], cv2.CV_64F, 0, 1)
+    e1 = np.sqrt(sobelx ** 2 + sobely **2)
+    e1[e1 < 0.1247] = 0
+    e1[e1 >= 0.1247] = 1
+    sobelx = cv2.Sobel(uv[:,:,1], cv2.CV_64F, 1, 0)
+    sobely = cv2.Sobel(uv[:,:,1], cv2.CV_64F, 0, 1)
+    e2 = np.sqrt(sobelx ** 2 + sobely **2)
+    e2[e1 < 0.1087] = 0
+    e2[e1 >= 0.1087] = 1
+    e  = np.logical_or(e1, e2)
+    mask = binary_dilation(e, np.ones(dilate_sz))
+    
+    # below to apply WMF to all regions
+    if fullVersion:
+        mask = np.ones(mask.shape)
+    indx_col, indx_row = np.where(mask.transpose() == 1)
+    bfhsz = int(bfhsz)
+    pad_u  = np.pad(uv[:,:,0], [bfhsz, bfhsz], 'symmetric') 
+    pad_v  = np.pad(uv[:,:,1], [bfhsz, bfhsz], 'symmetric')
+    pad_im = np.pad(im, ((bfhsz, bfhsz), (bfhsz, bfhsz), (0, 0)), 'symmetric')
+    pad_occ= np.pad(occ, [bfhsz, bfhsz], 'symmetric') 
+    
+    H, W = pad_u.shape
+    
+    # Divide into several groups for memory reasons ~70,000 causes out of memory
+    Indx_Row = indx_row
+    Indx_Col = indx_col
+    N        = len(Indx_Row) # number of elements to process
+    n        = int(4e4)              # number of elements per batch
+    nB       = int(np.ceil(N / n))
+    
+    for ib in range(1, nB + 1):
+        istart = (ib - 1) * n #+ 1
+        iend   = min(ib * n, N)
+        indx_row = Indx_Row[istart:iend + 1]
+        indx_col = Indx_Col[istart:iend + 1]
+    
+        C, R = np.meshgrid(range(-bfhsz, bfhsz + 1), range(-bfhsz, bfhsz + 1))
+        nindx = R + C * H
+        cindx = indx_row + bfhsz + (indx_col + bfhsz) * H
+        pad_indx = np.tile(nindx.transpose().flatten().reshape(-1, 1), [1, len(indx_row)]) +\
+                   np.tile(cindx.flatten().transpose(), [(bfhsz * 2 + 1) ** 2, 1])
+               
+        # spatial weight
+        tmp = np.exp(- (C ** 2 + R ** 2) / 2 / sigma_x ** 2)
+        weights = np.tile(tmp.flatten().reshape(-1, 1), [1, len(indx_row)])
+        
+        # Uncomment below: no spatial weight for test
+        # weights = np.ones(weights.shape)
+        
+        # Intensity weight
+        tmp_w = np.zeros(weights.shape)
+        
+        flat_cindx = cindx.flatten()
+        for i in range(pad_im.shape[2]):
+            tmp = pad_im[:,:,i].flatten()
+            tmp_w = tmp_w + (tmp[pad_indx] - np.tile(tmp[flat_cindx].transpose(), [(bfhsz * 2 + 1) ** 2, 1])) ** 2
+        
+        tmp_w = tmp_w / pad_im.shape[2]
+        
+        weights = weights * np.exp(-tmp_w / 2 / sigma_i ** 2)
+    
+        # Occlusion weight    
+        weights = weights * pad_occ.flatten()[pad_indx]
+        
+        # Normalize
+        weights = weights / np.tile(np.sum(weights, axis=0), [(bfhsz * 2 + 1) ** 2, 1])
+        
+        neighbors_u = pad_u.transpose().flatten()[pad_indx]
+        neighbors_v = pad_v.transpose().flatten()[pad_indx]
+        
+    
+        # solve weighted median filtering
+        uo   = uvo[:,:,0]
+        u    = weighted_median(weights, neighbors_u)
+        uo[(indx_row, indx_col)] = u
+        vo   = uvo[:,:,1]
+        v    = weighted_median(weights, neighbors_v)
+        vo[(indx_row, indx_col)] = v
+        uvo = np.stack([uo, vo], axis=2)
+        
+    return uvo
+
+
+def detect_occlusion(w, images, sigma_d=0.3, sigma_i=20):
+    """
+    Detect occlusion regions using flow divergence and brightness constancy
+    error
+    
+    the output taks continous value 
+    close to 0: occluded
+    close to 1: nonoccluded
+    
+    according to Appendix A.2 Sand & Teller "Particle Video" IJCV 2008
+    """
+    div = np.gradient(w[:,:,0], axis=0) + np.gradient(w[:,:,1], axis=1)
+    
+    div[div > 0] = 0
+    
+    It, _, _ = partial_deriv(images, w);
+    occ = np.exp(- div ** 2 / 2 / sigma_d ** 2) * np.exp(-It ** 2 / 2 / sigma_i ** 2)
+    return occ
+
+
+def post_process(w, I1, I2, sigmaS, sigmaC):
+    stack = np.stack([I1,I2], axis=2)
+    occ = detect_occlusion(w, stack)
+    wOut = denoise_color_weighted_medfilt2d(w, stack, medfilt2d(occ, [9, 9]), int(np.ceil(sigmaS)), 3, int(np.ceil(sigmaC)), int(np.ceil(sigmaS)), True)
+    return wOut
+
+
+def default_parameters():
+    #lmbd stands for lambda which is a build in in python
+    param = {'lmbd':1000, 'gamma':100, 'mu':0.1, 'nu':0.1, 'c2fSpacing':1.5, 'threshMatch':70, 'occThresh':0.5,'consistencyTol':2,'changeTol':1e-3, 'nbWarps':1, 'sigmaS':1, 'sigmaC':200, 'maxIters':150, 'iWM':50}
+    return param
+
+def midway(u_1, u_2):
+    u_midway_1 = np.zeros(u_1.shape)
+    u_midway_2 = np.zeros(u_2.shape)
+
+    index_u_1 = np.argsort(u_1, axis=None)
+    index_u_2 = np.argsort(u_2, axis=None)
+    u_sort_1 = u_1.flatten()[index_u_1]
+    u_sort_2 = u_2.flatten()[index_u_2]
+
+    u_midway_1[np.unravel_index(index_u_1, u_1.shape)] = (u_sort_1 + u_sort_2) / 2
+    u_midway_2[np.unravel_index(index_u_2, u_2.shape)] = (u_sort_1 + u_sort_2) / 2
+
+    u_midway_1 = u_midway_1.reshape(u_1.shape)
+    u_midway_2 = u_midway_2.reshape(u_2.shape)
+
+    return [u_midway_1, u_midway_2]
+
+def crop_fit_size_center(f, target_size):
+    sf = np.array(f.shape)
+    shift = np.around((sf - np.array(target_size)) / 2).astype(np.int_)
+    f_out = f[shift[0]:shift[0] + target_size[0], shift[1]:shift[1] + target_size[1], shift[2]:shift[2] + target_size[2]]
+    return f_out
+
+def eigsDtD(sx, sy, lmbd, mu):
+    res = [1. ,1.]
+    a = np.zeros((sx, sy))
+    a[0, 0] = 2 / res[0] ** 2 + 2 / res[1] ** 2
+    a[0, 1] = -1 / res[1] ** 2
+    a[1, 0] = -1 / res[0] ** 2
+    a[0, -1] = -1 / res[1] ** 2
+    a[-1, 0] = -1 / res[0] ** 2
+    fGtG = np.fft.fftn(a)
+    return lmbd * np.real(fGtG) ** 2 + mu
