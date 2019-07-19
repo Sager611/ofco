18c1d3a2ce271b7dc23745a94825019149d96c7d
faymanns <florian.aymanns@epfl.ch>
2019-07-19 15:45:41 +0200

    all tries for multiprocessing

--- ofco/main.py
+++ ofco/main.py
@@ -14,7 +14,8 @@ Options:
 """
 
 import os
-import multiprocessing
+import multiprocessing as mp
+import array
 from timeit import default_timer as timer
 import numpy as np
 from skimage import io
@@ -25,6 +26,12 @@ from .warping import bilinear_interpolate
 from .optflow import optical_flow_estimation
 
 
+#class NpArrayManager(mp.BaseManager):
+#    pass
+#
+#NpArrayManager.register('NpArray', np.array)
+
+
 def compute_motion(I1, I2, param):
     sz0 = I1.shape
 
@@ -32,16 +39,32 @@ def compute_motion(I1, I2, param):
     I2 = np.pad(I2, [15, 15], "edge")
 
     # Optical flow
+    #io.imsave('parallel_I1.tif',I1.astype(np.float32))
+    #io.imsave('parallel_I2.tif',I2.astype(np.float32))
+    #io.imsave('sequencial_I1.tif',I1.astype(np.float32))
+    #io.imsave('sequencial_I2.tif',I2.astype(np.float32))
     w = optical_flow_estimation(I1, I2, sz0, param)
 
     w = crop_fit_size_center(w, [sz0[0], sz0[1], 2])
     return w
 
 
-def parallel_compute_motion(t):
-    i2 = global_stack1_rescale[t + 1, :, :]
-    [i10, i2] = midway(global_i1, i2)
-    return compute_motion(i10, i2, global_param)
+def parallel_compute_motion(t, m_array, w):
+    i2 = global_stack1_rescale[t + 1, :, :].copy()
+    [i10, i2] = midway(global_i1.copy(), i2)
+    #return compute_motion(i10, i2, global_param)
+    #io.imsave(f'parallel_i10_{t}.tif',i10.astype(np.float32))
+    #io.imsave(f'parallel_i2_{t}.tif',i2.astype(np.float32))
+    start = timer()
+    #return_array[t] = compute_motion(i10, i2, global_param)
+    res_w = compute_motion(i10, i2, global_param)
+    end = timer()
+    print(f'Time for {t} compute motion is {end - start} s')
+    m_array.acquire()
+    w[t] = res_w
+    m_array.release()
+    #w_size = w.shape[0] * w.shape[1] * w.shape[2]
+    #return_array[t * w_size:(t + 1) * w_size] = array.array('d', w.flatten().tolist())
 
 
 def motion_compensate(
@@ -59,13 +82,11 @@ def motion_compensate(
 
     # Motion estimation
     start = timer()
-    w = np.zeros(
-        [
+    w_shape = (
             stack1_rescale.shape[0] - 1,
             stack1_rescale.shape[1],
             stack1_rescale.shape[2],
             2,
-        ]
     )
 
     i1 = stack1_rescale[frames[0], :, :]
@@ -76,16 +97,51 @@ def motion_compensate(
         global_i1 = i1
         global global_param
         global_param = param
-        pool = multiprocessing.Pool()
-        output = pool.map(parallel_compute_motion, range(len(frames) - 1))
-        w = np.array(output)
+        #with mp.Pool(2) as pool:
+        #    output = pool.map(parallel_compute_motion, range(len(frames) - 1))
+        #w = np.array(output)
+        processes = []
+        m_array = mp.Array('d', int(np.prod(w_shape)), lock=mp.Lock())
+        w = np.frombuffer(m_array.get_obj(), dtype='d').reshape(w_shape)
+        for i in range(len(frames) - 1):
+            #p = mp.Process(target=parallel_compute_motion, args=(i, return_array))
+            #p = mp.Process(target=parallel_compute_motion, args=(i, global_w))
+            p = mp.Process(target=parallel_compute_motion, args=(i, m_array, w))
+            processes.append(p)
+
+        for i in range(1, len(processes), 2):
+            processes[i - 1].start()
+            processes[i].start()
+            processes[i - 1].join()
+            processes[i].join()
+        if len(processes) % 2:
+            processes[-1].start()
+            processes[-1].join()
+
+        #started = 0
+        #joined = 0
+        #while started < len(processes):
+        #    if started - joined < 2:
+        #        processes[started].start()
+        #        started += 1
+        #    if processes[joined].is_alive():
+
+
+        #while any([proc.is_alive() for proc in processes]):
+        #for p in processes:
+        #    p.join()
     else:
+        w = np.zeros(w_shape)
         for t in range(len(frames) - 1):
             if verbose:
                 print("Frame {}\n".format(t))
             i2 = stack1_rescale[t + 1, :, :]
             [i10, i2] = midway(i1, i2)
+            #io.imsave(f'sequencial_i10_{t}.tif',i10.astype(np.float32))
+            #io.imsave(f'sequencial_i2_{t}.tif',i2.astype(np.float32))
             w[t, :, :, :] = compute_motion(i10, i2, param)
+    #assert(np.allclose(w, global_w))
+    #assert(np.allclose(w, w_parallel))
     end = timer()
     if verbose:
         print("Time it took to compute motion field w {}".format(end - start))
