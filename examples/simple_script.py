90764fe28e4405dde56180aaf463b449319dcd3a
faymanns <florian.aymanns@epfl.ch>
2019-07-19 16:40:25 +0200

    fixed typo

--- examples/simple_script.py
+++ examples/simple_script.py
@@ -6,10 +6,19 @@ stack1 = io.imread(
     "/mnt/internal_hdd/aymanns/181220_Rpr_R57C10_GC6s_tdTom/Fly5/002_coronal/2p/red.tif"
 )
 stack2 = io.imread(
-    "/mnt/internal_hdd/aymanns/181220_Rpr_R57C10_GC6s_tdTom/Fly5/002_coronal/2p/red.tif"
+    "/mnt/internal_hdd/aymanns/181220_Rpr_R57C10_GC6s_tdTom/Fly5/002_coronal/2p/green.tif"
 )
 
 param = default_parameters()
-frames = range(2)
+frames = range(20)
 
-motion_compensate(stack1, stack2, "warped1.tif", "warped2.tif", frames, param)
+motion_compensate(
+    stack1,
+    stack2,
+    "warped1.tif",
+    "warped2.tif",
+    frames,
+    param,
+    parallel=True,
+    verbose=True,
+)
