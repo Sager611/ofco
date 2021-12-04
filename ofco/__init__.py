712cecaded2313a130317911e2a505d004ced654
Sager611 <adriansagerlaganga@gmail.com>
2021-12-04 01:21:25 +0100

    created thread-parallel branch

--- ofco/__init__.py
+++ ofco/__init__.py
@@ -1,4 +1,26 @@
+import logging
 from .main import *
 
 __author__ = "Florian Aymanns"
 __email__ = "florian.aymanns@epfl.ch"
+
+#####################################################################
+# Adrian Sager 15/11/2021:
+#     Added logger
+#
+#####################################################################
+def initialize():
+    if '_ofco_initialized' in globals():
+        return
+    global _ofco_initialized
+    _ofco_initialized = True
+
+    logger = logging.getLogger('ofco')
+    logger.setLevel(logging.INFO)
+    stream_handler = logging.StreamHandler()
+    formatter = \
+        logging.Formatter('[%(asctime)s] %(levelname).1s T%(thread)d %(filename)s:%(lineno)s: %(message)s')
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+initialize()
